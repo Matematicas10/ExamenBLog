13c1e1d271b49fdc2dd9c4a7578df5802e11eba0
Matematicas10 <15209453@modelo.edu.mx>
2022-12-07 19:19:47 -0600

    otra bendición

--- PHP/Registro.php
+++ PHP/Registro.php
@@ -11,8 +11,10 @@ $fecha=$_POST['fecha'];
 echo "Los datos son los siguientes: <br>";
 echo "$titulo,$imagen,$descripcion,$autor y $fecha";
 
+
 $conn=conn();//hace la conexión de base de datos 
 $sql="INSERT INTO `articulos`(`titulo`, `imagen`, `descripcion`, `autor`, `fecha`) VALUES ('$titulo','$imagen','$descripcion','$autor','$fecha')";
 $resul= mysqli_query($conn,$sql)or trigger_error("Query Failed SQL- Error:".mysqli_error($conn), E_USER_Error);
-
-?>
+header("Location: http://localhost/Ex%C3%A1men/ini.php");
+exit();
+?>,
